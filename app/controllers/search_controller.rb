b163285bc6b4b4c004a590db32d2b1a51b9e0a37
Andrew Dymond <andy.dymond@gmail.com>
2018-06-25 16:41:36 -0600

    Replace controller logic with flickr_service call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,8 @@
 class SearchController < ApplicationController
 
   def index
-    conn = Faraday.new
-    response = conn.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{ENV['flickr_api_key']}&format=json&nojsoncallback=1&safe_search=1&text=#{params[:search_field]}")
-    results = JSON.parse(response.body, symbolize_names: true)
-    @image_urls = results[:photos][:photo].map do |data|
-      "http://farm#{data[:farm]}.static.flickr.com/#{data[:server]}/#{data[:id]}_#{data[:secret]}.jpg"
-    end
+    fs = FlickrService.new
+    @image_urls = fs.urls('cats')
     flash[:notice] = "displaying search results for #{params[:search_field]}"
   end
 
