397649989ef2a8f498d3569270a2f710198ad314
Andrew Dymond <andy.dymond@gmail.com>
2018-06-25 16:51:12 -0600

    Pass search term to service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
   def index
     fs = FlickrService.new
-    @image_urls = fs.urls('cats')
+    @image_urls = fs.urls(params[:search_field])
     flash[:notice] = "displaying search results for #{params[:search_field]}"
   end
 
